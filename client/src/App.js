338bb70b63ccb9a35b9997b06e2798ea62d9e1d7
Alex Pham <925workk@gmail.com>
2020-01-27 18:10:53 -0700

    post pickups class

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,21 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NewAcct from "./pages/NewAcct";
 import Profile from "./pages/Profile";
+import Postpickups from "./pages/Postpickups";
 
 class App extends Component {
-  
   render() {
     return (
       <Router>
       <div>
+        
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/create" component={NewAcct} />
+          <Route exact path="/postpickup" component={Postpickups}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange} />
         </Switch>
       </div>
     </Router>
